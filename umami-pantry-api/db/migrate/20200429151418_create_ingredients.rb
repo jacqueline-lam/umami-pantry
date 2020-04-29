63e1ab64ff792b28656272654136e2dcb8608b6f
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-04-29 12:33:43 -0400

    add image_url column  to ingredient migration

--- umami-pantry-api/db/migrate/20200429151418_create_ingredients.rb
+++ umami-pantry-api/db/migrate/20200429151418_create_ingredients.rb
@@ -3,7 +3,7 @@ class CreateIngredients < ActiveRecord::Migration[6.0]
     create_table :ingredients do |t|
       t.string :name
       t.string :category
-
+      t.string :image_url
       t.timestamps
     end
   end
