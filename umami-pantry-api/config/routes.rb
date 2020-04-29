2ffebbb124fd0aca36a12f600efcaf93398e8865
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-04-29 15:31:11 -0400

    set up routes for rails api

--- umami-pantry-api/config/routes.rb
+++ umami-pantry-api/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :recipe_ingredients
-  resources :ingredients
+  resources :recipes, only: [:index, :create, :show]
+  resources :ingredients, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
