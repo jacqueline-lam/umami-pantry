a306ac70c00f81eecd0687194b63a048da0861f2
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-04-30 03:25:43 -0400

    create index action in IngredientsController and redner json

--- umami-pantry-api/app/controllers/ingredients_controller.rb
+++ umami-pantry-api/app/controllers/ingredients_controller.rb
@@ -1,2 +1,9 @@
 class IngredientsController < ApplicationController
+  def index
+    # Render json (js format used by AJAX lib)
+      # convert obj to JSON and render JSON back to browser
+    # @ingredients = Ingredient.all
+    render json: Ingredient.all
+  end
+
 end
