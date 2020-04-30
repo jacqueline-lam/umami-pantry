bdf815b0c75d561ec00fb23a3b74298c857e8db9
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-04-30 14:45:21 -0400

    add requirements to readme.md; add commnets

--- umami-pantry-api/app/controllers/ingredients_controller.rb
+++ umami-pantry-api/app/controllers/ingredients_controller.rb
@@ -1,7 +1,7 @@
 class IngredientsController < ApplicationController
   def index
     # Render json (js format used by AJAX lib)
-      # convert obj to JSON and render JSON back to browser
+      # convert objects from ORM to JSON and render JSON back to browser
     # @ingredients = Ingredient.all
     render json: Ingredient.all
   end
