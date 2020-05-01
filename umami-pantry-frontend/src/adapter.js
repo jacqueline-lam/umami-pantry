d18e39ac0e19d8adada7482f8a26e533d42128e3
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-05-01 15:59:27 -0400

    creatte method names for Adapter class objects

--- umami-pantry-frontend/src/adapter.js
+++ umami-pantry-frontend/src/adapter.js
@@ -1,16 +1,31 @@
-// rename to Adapter?
+// rename to AppContainer?
+// serves as in browser data storage system
+
 class Adapter {
   constructor() {
     this.baseUrl = 'http://localhost:8000/'
     this.ingredientsUrl = `${baseUrl}/ingredients`
     this.recipesUrl = `${baseUrl}/recipes`
   }
+  ingredients = []
+  categories = []
+  recipeResults = {}
 
   // make a fetch request to recipesUrl
-  // once we get back a resp, parse JSON from response 
+  // once we get back a resp, parse JSON from response
   getIngredients() {
     return fetch(this.recipesUrl)
       .then(resp => resp.json())
+  }
+
+  getRecipes(){
+    // make a fetch request to /recipes
+    // populate recipe and ingredient properties with returned data
+    // call renderRecipes
+  }
+
+  getRecipes(){
+    //create DOM nodes and insert data into them to render in the DOM
   }
 }
 
