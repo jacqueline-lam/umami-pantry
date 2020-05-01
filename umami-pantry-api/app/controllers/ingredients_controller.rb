ad88cd847b1b3757473e4663d3b1eda6d5cd31e0
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-05-01 16:33:30 -0400

    add  show RecipesController action to render json

--- umami-pantry-api/app/controllers/ingredients_controller.rb
+++ umami-pantry-api/app/controllers/ingredients_controller.rb
@@ -2,7 +2,7 @@ class IngredientsController < ApplicationController
   def index
     # Render json (js format used by AJAX lib)
       # convert objects from ORM to JSON and render JSON back to browser
-    # @ingredients = Ingredient.all
+    # ingredients = Ingredient.all
     render json: Ingredient.all
   end
 
--- umami-pantry-api/app/controllers/recipes_controller.rb
+++ umami-pantry-api/app/controllers/recipes_controller.rb
@@ -1,2 +1,20 @@
 class RecipesController < ApplicationController
+  def index
+    # Render json (js format used by AJAX lib)
+      # convert objects from ORM to JSON and render JSON back to browser
+    render json: Recipe.all
+  end
+
+  def show
+    recipe = Recipe.find_by(id: params[:id])
+    if recipe
+      # include - API to send a resource's data along with its associated resources' data
+      render json: recipe.to_json(include: {
+        ingredients: { only: :name },
+        recipe_ingredients: { only: [:amount, :preparation_method] }
+      }, except: [:created_at, :updated_at])
+    else
+      render json: { message: 'Recipe not found.' }
+    end
+  end
 end
